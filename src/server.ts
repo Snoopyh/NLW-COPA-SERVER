24c7015bcadd6afddb323bde39d06083cdf03521
Snoopyh <ismaelxd500@gmail.com>
2022-11-03 03:50:19 -0300

    last changes

--- src/server.ts
+++ src/server.ts
@@ -1,55 +1,61 @@
-import Fastify from "fastify";
-import cors from "@fastify/cors";
-import { z } from "zod";
-import { PrismaClient } from "@prisma/client";
-import ShortUniqueId from "short-unique-id";
+import Fastify from "fastify"
+import cors from "@fastify/cors"
+import { z } from "zod"
+import { PrismaClient } from "@prisma/client"
+import ShortUniqueId from "short-unique-id"
 
 const prisma = new PrismaClient({
-  log: ["query"],
-});
+  log: ['query'],
+})
+
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
-  });
+  })
 
   await fastify.register(cors, {
     origin: true,
-  });
+  })
 
-  fastify.get("/pools/count", async () => {
-    const count = await prisma.pool.count();
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count()
 
-    return { count };
-  });
-  fastify.get("/users/count", async () => {
-    const count = await prisma.pool.count();
+    return { count }
+  })
 
-    return { count };
-  });
-  fastify.get("/guesses/count", async () => {
-    const count = await prisma.pool.count();
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count()
 
-    return { count };
-  });
+    return { count }
+  })
 
-  fastify.post("/pools", async (request, reply) => {
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count()
+
+    return { count }
+  })
+
+  fastify.post('/pools', async (request, reply) => {
     const createPoolBody = z.object({
       title: z.string(),
-    });
+    })
+
     const { title } = createPoolBody.parse(request.body);
-    const genarate = new ShortUniqueId({ length: 6 });
-    const code = String(genarate()).toUpperCase();
+
+    const generate = new ShortUniqueId({ length: 6 })
+    const code = String(generate()).toUpperCase()
 
     await prisma.pool.create({
       data: {
         title,
         code,
-      },
-    });
+      }
+    })
 
-    return reply.status(201).send({ title, code });
-  });
+    return reply.status(201).send({ code })
+  })
 
-  await fastify.listen({ port: 3333 /*ost:'0.0.0.0'*/ });
+  await fastify.listen({ port: 3333, /* host: '0.0.0.0' */ })
 }
-bootstrap();
+
+bootstrap()
