03408278ca1b9961e686838b7e595c749ba2a34e
Snoopyh <ismaelxd500@gmail.com>
2022-11-05 15:34:05 -0300

    Ajustes server

---
+++ prisma/migrations/20221105020000_create_table/migration.sql
@@ -0,0 +1,53 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createAdt` on the `Pool` table. All the data in the column will be lost.
+  - You are about to drop the column `CreateAdt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `CreateAdt` on the `Guess` table. All the data in the column will be lost.
+  - You are about to drop the column `secontTeamPoints` on the `Guess` table. All the data in the column will be lost.
+  - Added the required column `secondTeamPoints` to the `Guess` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Pool" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ownerId" TEXT,
+    CONSTRAINT "Pool_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Pool" ("code", "id", "ownerId", "title") SELECT "code", "id", "ownerId", "title" FROM "Pool";
+DROP TABLE "Pool";
+ALTER TABLE "new_Pool" RENAME TO "Pool";
+CREATE UNIQUE INDEX "Pool_code_key" ON "Pool"("code");
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "googleId" TEXT,
+    "avatarUrl" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_User" ("avatarUrl", "email", "id", "name") SELECT "avatarUrl", "email", "id", "name" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
+CREATE TABLE "new_Guess" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "firstTeamPoints" INTEGER NOT NULL,
+    "secondTeamPoints" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "gameId" TEXT NOT NULL,
+    "participantId" TEXT NOT NULL,
+    CONSTRAINT "Guess_participantId_fkey" FOREIGN KEY ("participantId") REFERENCES "Participant" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Guess_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Guess" ("firstTeamPoints", "gameId", "id", "participantId") SELECT "firstTeamPoints", "gameId", "id", "participantId" FROM "Guess";
+DROP TABLE "Guess";
+ALTER TABLE "new_Guess" RENAME TO "Guess";
+CREATE UNIQUE INDEX "Guess_participantId_gameId_key" ON "Guess"("participantId", "gameId");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
