f16af3c434407a29ad8b58813bc5388566d44bf3
Snoopyh <ismaelxd500@gmail.com>
2022-11-07 00:57:31 -0300

    Ajustes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,51 +1,68 @@
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
-async function main(){
+async function main() {
   const user = await prisma.user.create({
     data: {
       name: 'John Doe',
-      email: 'johndoe@gmail.com',
-      avatarUrl: 'https://github.com/Snoopyh.png',
+      email: 'john.doe@gmail.com',
+      avatarUrl: 'https://github.com/diego3g.png',
     }
-  })
+  });
 
-  const pool  = await prisma.pool.create({
-    data : {
-      title:'Exemplo',
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Example Pool',
       code: 'BOL123',
       ownerId: user.id,
 
-      participants:{
-        create:{
-          userId: user.id
+      participants: {
+        create: {
+          userId: user.id,
         }
       }
     }
-  })
+  });
 
   await prisma.game.create({
-    data:{
-      date: '2022-11-02T12:00:28.280Z',
-      firstTeamCountryCode:'DE',
-      secondTeamCountryCode:'BR',
+    data: {
+      date: '2022-11-02T12:00:00.201Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR',
     }
   })
+
   await prisma.game.create({
-    data:{
-      date: '2022-11-03T12:00:28.280Z',
-      firstTeamCountryCode:'AR',
-      secondTeamCountryCode:'US',
+    data: {
+      date: '2022-11-02T12:00:00.201Z',
+      firstTeamCountryCode: 'AR',
+      secondTeamCountryCode: 'PT',
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-02T12:00:00.201Z',
+      firstTeamCountryCode: 'UR',
+      secondTeamCountryCode: 'CO',
+    }
+  })
+  
+  await prisma.game.create({
+    data: {
+      date: '2022-11-03T12:00:00.201Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
 
       guesses: {
-        create:{
-          firstTeamPoints:3,
-          secontTeamPoints:2,
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
 
-          participant:{
+          participant: {
             connect: {
-              userId_poolId:{
+              userId_poolId: {
                 userId: user.id,
                 poolId: pool.id,
               }
@@ -53,7 +70,8 @@ async function main(){
           }
         }
       }
-    }
+    },
   })
 }
+
 main()
